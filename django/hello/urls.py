7bd5f5dc6318ead055ee5cc396ea34f40dbf4716
Jeff Kim <eunkeekim@gmail.com>
2020-06-13 02:55:29 +0900

    passing variables to template files

--- django/hello/urls.py
+++ django/hello/urls.py
@@ -5,8 +5,7 @@ urlpatterns = [
     # path(url, which handler, name to reference from other parts of the app)
     # localhost:8000/hello
     path("", views.index, name="index"),
-    # localhost:8000/hello/jeff
-    path("jeff", views.jeff, name="Jeff"),
+    path("time", views.currentDate, name="time"),
     # Accepts any string with its parameter in name 
     path("<str:name>", views.greet, name="greet"),
     # path("calculator/<int:first>/<int:second>", views.calculator, name="calculator"),
--- django/hello/views.py
+++ django/hello/views.py
@@ -1,16 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request): 
     # return HttpResponse("Hello world from hello project")
-    return render(request, "index.html")
-
-def jeff(request): 
-    return HttpResponse("Hi from jeff")
+    return render(request, "hello/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello {name.capitalize()}")
+    # return HttpResponse(f"Hello {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
 
 def calculator(request, first, second):
     return HttpResponse(first + second)
+
+def currentDate(request): 
+    curr = datetime.now() 
+    print(curr)
+    return render(request, "hello/time.html", {
+        "monday": datetime.today().strftime("%A") == "Monday",
+        "tuesday": datetime.today().strftime("%A") == "Tuesday",
+        "wednesday": datetime.today().strftime("%A") == "Wednesday",
+        "thursday": datetime.today().strftime("%A") == "Thursday",
+        "friday": datetime.today().strftime("%A") == "Friday",
+        "saturday": datetime.today().strftime("%A") == "Saturday",
+        "sunday": datetime.today().strftime("%A") == "Sunday"
+    })
