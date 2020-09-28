cf6bda2fcab07f95084c4a1e4db72aa487533cfd
Jeff Kim <eunkeekim@gmail.com>
2020-09-28 00:35:07 -0400

    update

--- basic-page/main.js
+++ basic-page/main.js
@@ -11,16 +11,63 @@ form.addEventListener("submit", (e) => {
 });
 
 async function getPlayerImage(firstName, lastName) {
-  console.log("Pokemon info is called");
-  let endpointURL = requestUrl + "players/" + lastName + "/" + firstName;
+  // console.log("Pokemon info is called");
+  // let endpointURL = requestUrl + "players/" + lastName + "/" + firstName;
+  // const res = await fetch(endpointURL); //GET request
+  // const data = await res.blob();
+  // const imgURL = URL.createObjectURL(data);
+  // document.querySelector("img").src = imgURL;
+  let endpointURL = "http://localhost:5000/test";
   const res = await fetch(endpointURL); //GET request
+  const data = await res.text();
+  console.log(data);
+}
+
+async function send_image(aa) {
+  let photo = document.getElementById("img-file").files[0];
+  // console.log(photo);
+  let formData = new FormData();
+
+  formData.append("file", photo);
+  // fetch("http://localhost:5000/upload", {
+  //   method: "POST",
+  //   body: formData,
+  // }).then((response) => console.log(response));
+  const res = await fetch("http://localhost:5000/upload", {
+    method: "POST",
+    body: formData,
+  });
   const data = await res.blob();
+  console.log(data);
   const imgURL = URL.createObjectURL(data);
   document.querySelector("img").src = imgURL;
 }
 
-async function ha() {
-  const a = await fetch("http://localhost:5000");
-  console.log(a);
-  document.getElementById("results").innerHTML = a.text();
-}
+// const imageUpload = document.getElementById("imageUpload");
+
+// imageUpload.onsubmit = (e) => {
+//   e.preventDefault();
+//   console.log(imageUpload.img.value);
+
+//   fetch("http://localhost:5000/upload", {
+//     // Adding method type
+//     method: "POST",
+
+//     // Adding body or contents to send
+//     body: JSON.stringify({
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     }),
+
+//     // Adding headers to the request
+//     headers: {
+//       "Content-type": "image/jpeg;",
+//     },
+//   })
+//     // Converting to JSON
+//     .then((response) => response.json())
+
+//     // Displaying results to console
+//     .then((json) => console.log(json));
+// };
--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, request, json, send_file, make_response
+from PIL import Image
+import base64
+from io import BytesIO
+from flask_cors import CORS
+
+# create an instance of a Flask class
+# First argument is the name of the application's module or package
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def hello():
-    return "hello"
+@app.route('/test', methods=['GET', 'POST'])
+# Set router for the url
+def hello_world():
+    a = {}
+    a["ha"] = "huh"
+    a["ha2"] = "huh2"
+
+    # resp = flask.Response("")
+    # response = json.dumps(a)
+
+    return a
 
 
-def photo():
-    return "photo.jpg"
+@app.route('/upload', methods=['POST', 'OPTIONS'])
+def upload():
+    f = request.files['file']
+    print(f)
+    img = Image.open(f)
+    img_io = BytesIO()
+    img.save(img_io, 'jpeg')
+    img_io.seek(0)
+
+    response = make_response(send_file(img_io, mimetype='image/jpeg'))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
