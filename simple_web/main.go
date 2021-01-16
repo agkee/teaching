e0164b5c2b5923e0cb178d858e9bc8b5339ea8d8
Jeff Kim <eunkeekim@gmail.com>
2021-01-16 11:39:59 -0500

    feat: Add greetings handler

--- simple_web/main.go
+++ simple_web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,10 +13,18 @@ import (
 
 func main() {
 
+	// localhost:8080/
+	http.HandleFunc("/", greetingHandler)
+
 	// Running the server
-	log.Println("Server is running on port haha :8080")
+	log.Println("Server is running on port :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Couldn't start server in port 8080 %v", err)
 	}
+}
+
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("User called default api")
+	fmt.Fprintf(w, "Hello world")
 }
